e17d3b57d698d45b6755a58ad3a5c527fe2c9444
Bo Molenaar <bo.molenaar@ru.nl>
2022-03-29 12:32:18 +0200

    First test tg file selection

--- data_sel_jasmin_G1.py
+++ data_sel_jasmin_G1.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-## Please, modify the TODO instance (x1) to establish 
-## the selection criteria (by default Group = 1)
 
 import re
 import os
@@ -10,61 +8,84 @@ import sys
 import shutil
 from pathlib import Path
 
-if (len(sys.argv) < 3):
-    print("You must add two arguments: a folder name for generating the files and the root path to this project folder.")
+if (len(sys.argv) < 4):
+    print("You must add three arguments: folder names for generating qG1-1 and qG1-2 files and the root path to this project folder.")
     sys.exit(-1)
 
 
-date = sys.argv[1]
-myfolder = os.path.join(sys.argv[2], date)
+subset1 = sys.argv[1]
+subset2 = sys.argv[2]
+main_folder = os.path.join(sys.argv[3], subset1)
+second_folder = os.path.join(sys.argv[3], subset2)
 jasmin_folder = '/vol/bigdata/corpora/JASMIN/'
 recordings = '/vol/bigdata/corpora/JASMIN/CDdoc/data/meta/text/nl/recordings.txt'
-selected_recordings = os.path.join(myfolder, 'rec_to_use.txt')
-wav_folder = os.path.join(myfolder, 'wav_files_to_use')
-wav_folder_train = os.path.join(myfolder, 'wav_files_to_use_train')
-wav_folder_test = os.path.join(myfolder, 'wav_files_to_use_test')
-ort_folder = os.path.join(myfolder, 'ort_files_to_use')
-hmi_folder = os.path.join(myfolder, 'hmi_files_to_use')
-awd_folder = os.path.join(myfolder, 'awd_files_to_use')
+textgrid_path = '/vol/tensusers4/bmolenaar/IS2022proj/Limonard_textgrids_utf8'
+selected_recordings = os.path.join(main_folder, 'rec_to_use.txt')
+wav_folder_train = os.path.join(second_folder, 'wav_files_to_use_train')
+wav_folder_test = os.path.join(main_folder, 'wav_files_to_use_test')
+ort_folder = os.path.join(main_folder, 'manual_transcriptions')
+prompt_folder = os.path.join(main_folder, 'prompts')
+second_ort_folder = os.path.join(second_folder, 'manual_transcriptions')
 
-# remove my old folder
-shutil.rmtree(myfolder, ignore_errors=True)
+# remove my old folders
+shutil.rmtree(main_folder, ignore_errors=True)
+shutil.rmtree(second_folder, ignore_errors=True)
 
-# create myfolder
-path_to_myfolder = Path(myfolder)
-path_to_myfolder.mkdir()
-praat_folder = os.path.join(myfolder,'praat_files_to_use')
-tier_folder =  os.path.join(myfolder,'tier')
-os.mkdir(praat_folder)
-os.mkdir(tier_folder)
+# create main_folder and second_folder
+path_to_main_folder = Path(main_folder)
+path_to_main_folder.mkdir()
+path_to_second_folder = Path(second_folder)
+path_to_second_folder.mkdir()
+
+def split_save_stories(textgrid_list, wav_folder_train, wav_folder_test, ort_folder, second_ort_folder, prompt_folder):
+    'wtf did i do here again?'
+
+    for textgrid in textgrid_list:
+        file = open(textgrid, 'r', encoding='utf8').readlines()
+        for line in range(len(file)):
+            if 'item [1]' in file[line]:
+                transcripts_start = line
+            if 'item [2]' in file[line]:
+                transcripts_end = line[-1]
+            if 'item [4]' in file[line]:
+                prompts_start = line
+            if 'item [5]' in file[line]:
+                prompts_end = line[-1]
+
+        # iterate over prompts
+        for line in range(prompts_start, prompts_end):
+            if ('text =' in file[line]) and ('name =' not in file[line]):
+                if re.findall('"([^"]*)"', file[line]) != "":
+                    text = file[line].split('=')[1].strip(' ')
+                    xmin = re.findall("\d+\.?\d*", file[line-2])
+                    xmax = re.findall("\d+\.?\d*", file[line-1])
+
 
 # generate selected recordings, change the if statement accordingly
 with open(recordings,'r', encoding='utf-8') as f_in, open(selected_recordings,'w', encoding='utf-8') as f_out:
     for line in f_in:
         w_lst = line.split()
-        if w_lst[3] == '1': # TODO groups or component (see columns in file "$recordings")
+        if (w_lst[3] == '1') and (w_lst[2] == 'comp-q'):
             f_out.write(line)
 
 # create folders if not exist, remove folders if exist
-myfolder_lst = [wav_folder, ort_folder, hmi_folder, awd_folder, wav_folder_train, wav_folder_test]
-for myfolder in myfolder_lst:
-    if os.path.isdir(myfolder):
-        filelist = [f for f in os.listdir(myfolder)]
+folder_lst = [prompt_folder, second_ort_folder, ort_folder, wav_folder_train, wav_folder_test]
+for folder in folder_lst:
+    if os.path.isdir(folder):
+        filelist = [f for f in os.listdir(folder) if not os.path.isdir(f)]
         for f in filelist:
-            os.remove(os.path.join(myfolder,f))
+            os.remove(os.path.join(folder,f))
     else:
-        os.mkdir(myfolder)
+        os.mkdir(folder)
 
-# put selected recordings .wav .ort .hmi and .awd in my folders
+# put selected recordings .wav my folder
 name_lst = []
 with open(selected_recordings, 'r', encoding='utf-8') as f:
     for line in f:
         name_lst.append(line.split()[0])
 
 wav_file_lst = []
-ort_file_lst = []
-hmi_file_lst = []
-awd_file_lst = []
+tg_file_lst = []
 # Cristian: this is not optimized at all... but it works :)
 for i in name_lst:
     for dirpath, dirnames, filenames in os.walk(jasmin_folder):
@@ -72,29 +93,18 @@ for i in name_lst:
             if i in filename:
                 if filename.endswith('.wav'):
                     wav_file_lst.append(os.path.join(dirpath, filename))
-                if filename.endswith('.ort'):
-                    ort_file_lst.append(os.path.join(dirpath, filename))
-                if filename.endswith('.hmi'):
-                    hmi_file_lst.append(os.path.join(dirpath, filename))
-                if filename.endswith('.awd'):
-                    awd_file_lst.append(os.path.join(dirpath, filename))
+    for dirpath, dirnames, filenames in os.walk(textgrid_path):
+        for filename in filenames:
+            if i in filename:
+                if filename.endswith('.TextGrid'):
+                    tg_file_lst.append(os.path.join(dirpath, filename))
 
 print('wav_file_lst',len(wav_file_lst))
 for j in wav_file_lst:
     #print(j)
     shutil.copy(j, wav_folder)
 
-print('ort_file_lst',len(ort_file_lst))
-for m in ort_file_lst:
-    #print(m)
-    shutil.copy(m, ort_folder)
+print('textgrid_file_lst',len(tg_file_lst))
+print(tg_file_lst[:5])
 
-print('hmi_file_lst',len(hmi_file_lst))
-for n in hmi_file_lst:
-    #print(n)
-    shutil.copy(n, hmi_folder)
-
-print('awd_file_lst',len(awd_file_lst))
-for q in awd_file_lst:
-    #print(q)
-    shutil.copy(q, awd_folder)
+#split_save_stories()
